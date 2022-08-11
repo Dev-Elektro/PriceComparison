3721e2d03eef8f2affaff81c9b61da2b3011cccf
Dev-Elektro <elektro.linux@gmail.com>
2022-08-11 15:49:41 +0300

    Добавлена графика для разового поиска.

---
+++ gui/__init__.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import win32net, pywintypes
+from PyQt5 import QtCore, QtWidgets, QtGui
+from . import mainwindow
+from .requestsearch import requestsearch
+
+
+class App(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle(f'Сравнение цен')
+        self.butSearch.clicked.connect(self.startSearch)
+        self.progressBar.hide()
+
+    def startSearch(self):
+        if len(self.lineSearch.text()) < 3:
+            return
+        self.thread = QtCore.QThread()
+        self.request = requestsearch(self.lineSearch.text())
+        self.request.moveToThread(self.thread)
+        self.thread.started.connect(self.request.run)
+        self.request.startSearch.connect(self.resultFrom)
+        self.request.found.connect(self.foundSearch)
+        self.request.finished.connect(self.finishSearch)
+        self.request.finished.connect(self.thread.quit)
+        self.request.finished.connect(self.request.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.butSearch.setText('Стоп')
+        self.butSearch.clicked.disconnect()
+        self.butSearch.clicked.connect(self.stopSearch)
+        self.textBrowser.setTextInteractionFlags (QtCore.Qt.NoTextInteraction)
+        self.textBrowser.clear()
+        self.thread.start()
+        self.statusbar.showMessage(f"Поиск: {self.lineSearch.text()}")
+        self.progressBar.setMaximum(0)
+        self.progressBar.setValue(0)
+        self.progressBar.show()
+
+    def stopSearch(self):
+        self.request.stop()
+        self.butSearch.setText('Остановка')
+        self.statusbar.showMessage("Остановка поиска")
+
+    def finishSearch(self, res):
+        self.progressBar.hide()
+        self.butSearch.setText('Поиск')
+        self.butSearch.clicked.disconnect()
+        self.butSearch.clicked.connect(self.startSearch)
+        self.textBrowser.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse | QtCore.Qt.TextSelectableByKeyboard | QtCore.Qt.TextBrowserInteraction)
+        self.statusbar.showMessage("Поиск завершен")
+        if not res:
+            self.textBrowser.insertHtml(f"<br><b>Поиск прерван</b><br>")
+
+    def resultFrom(self, name):
+        if self.textBrowser.toPlainText():
+            self.textBrowser.insertHtml(f"<br>")
+        self.textBrowser.insertHtml(f"<b>Результат с сайта {name}</b><br><br>")
+        self.progressBar.setMaximum(3)
+        self.progressBar.setValue(self.progressBar.value() + 1)
+
+    def foundSearch(self, res):
+        if not res:
+            self.textBrowser.insertHtml(f"Нет результата.<br><br>")
+            return
+        self.textBrowser.insertHtml(f"<a href='{res.get('link')}'>{res.get('name')} - Цена: {res.get('price')}</a><br>")
+
+def startGUI():
+    app = QtWidgets.QApplication(sys.argv)
+    translator = QtCore.QTranslator()
+    if hasattr(sys, '_MEIPASS'):
+        if translator.load(os.path.join(sys._MEIPASS, "qtbase_ru.qm")):
+            app.installTranslator(translator)
+    window = App()
+    window.show()
+
+    app.exec_()
---
+++ gui/requestsearch.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+from PyQt5 import QtCore
+sys.path.append("..")
+from searchSite import Driver, citilink, regard, dnsshop, ozon
+
+class requestsearch(QtCore.QObject):
+    finished = QtCore.pyqtSignal(object)
+    found = QtCore.pyqtSignal(object)
+    startSearch = QtCore.pyqtSignal(object)
+
+    def __init__(self, request):
+        super().__init__()
+        self.request = request
+        self.stoped = False
+        self._mutex = QtCore.QMutex()
+        self.driver = Driver(headless = False)
+
+    def stop(self):
+        self._mutex.lock()
+        self.stoped = True
+        self._mutex.unlock()
+
+    def _stopSearch(self):
+        self.driver.stop()
+        self.finished.emit(False)
+
+    def run(self):
+        self.driver.start()
+
+        if self.stoped: return self._stopSearch()
+        buf = citilink.search(self.driver, self.request)
+        self.startSearch.emit("citilink.ru")
+        f = False
+        for res in buf:
+            if self.stoped: return self._stopSearch()
+            f = True
+            self.found.emit(res)
+        if not f:
+            self.found.emit(None)
+
+        if self.stoped: return self._stopSearch()
+        buf = regard.search(self.driver, self.request)
+        self.startSearch.emit('regard.ru')
+        f = False
+        for res in buf:
+            if self.stoped: return self._stopSearch()
+            f = True
+            self.found.emit(res)
+        if not f:
+            self.found.emit(None)
+
+        if self.stoped: return self._stopSearch()
+        buf = dnsshop.search(self.driver, self.request)
+        self.startSearch.emit('dns-shop.ru')
+        f = False
+        for res in buf:
+            if self.stoped: return self._stopSearch()
+            f = True
+            self.found.emit(res)
+        if not f:
+            self.found.emit(None)
+        driver.stop()
+        self.finished.emit(True)
