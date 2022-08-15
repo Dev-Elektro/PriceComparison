1419302cc278e8bd569deb50df4428cd0b23d906
Dev-Elektro <elektro.linux@gmail.com>
2022-08-15 12:37:00 +0300

    Внесены корректировки по условию ТЗ

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def search(func, cells):
     driver.start()
     result = []
     for cell in cells:
-        res = [i for i in func(driver, cell.value)] # Вызывов функции поиска с передачей вебдрайвера и текста поискового запроса. Результат собираем в список.
-        result.append({'row': cell.row, 'txt': cell.value, 'res': res}) # Формируем словарь с номером строки ячейки, текстом запроса и результатом поиска.
+        res = [i for i in func(driver, cell.get('value'))] # Вызывов функции поиска с передачей вебдрайвера и текста поискового запроса. Результат собираем в список.
+        result.append({'row': cell.get('row'), 'txt': cell.get('value'), 'res': res}) # Формируем словарь с номером строки ячейки, текстом запроса и результатом поиска.
     driver.stop()
     return result
 
@@ -20,12 +20,17 @@ def main():
     filePath = 'D:/MyProject/Итоговый реестр ШАБЛОН_2.xlsx'
     file = load_workbook(filePath) # Загружаем в память книгу xlsx
     sheet = file['Отчет'] # Открываем лист книги
-    cells = list(filter(lambda x: not x.value is None, sheet['C'][6:])) # Выборка всех яцеек с запросами из столбца 'C' начиная с 6 строки
+    cells = list(filter(lambda x: x.data_type == 's', sheet['B'][6:])) # Выборка всех яцеек с запросами из столбца 'B' начиная с 6 строки
+    searchQuery = []
+    for cell in cells: # Бежим по столбцу необходимых позиций
+        cellFootnote = sheet.cell(row = cell.row, column = columnIndex('C')) # Получаем ячейку примечание с текущей строки
+        query = cellFootnote.value if cellFootnote.data_type == 's' else cell.value # Берем значение со столбца примечание, если не заполнено берем значение со столбца необходимых позиций
+        searchQuery.append({'row': cell.row, 'value': query})
     pool = ThreadPool(processes=4) # Инициализация пула на 4 потока.
     processes = [
-        {'column': 'H', 'proc': pool.apply_async(search, [citilink.search, cells])}, # Славарь с указанием колонки для записи результата и ссылкой на поток поиска по сайту.
-        {'column': 'N', 'proc': pool.apply_async(search, [dnsshop.search, cells])},
-        {'column': 'T', 'proc': pool.apply_async(search, [regard.search, cells])},
+        {'column': 'H', 'proc': pool.apply_async(search, [citilink.search, searchQuery])}, # Славарь с указанием колонки для записи результата и ссылкой на поток поиска по сайту.
+        {'column': 'N', 'proc': pool.apply_async(search, [dnsshop.search, searchQuery])},
+        {'column': 'T', 'proc': pool.apply_async(search, [regard.search, searchQuery])},
     ]
     for proc in processes:
         for result_proc in proc.get('proc').get(): # Получаем результат выполнения потока
